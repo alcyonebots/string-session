1b56c155b0cca2aadd91dbc2576c3cdeba7ff633
alcyonebots <evedhatachi@gmail.com>
2025-01-19 20:48:44 +0530

    Update env.py

--- env.py
+++ env.py
@@ -5,10 +5,8 @@ load_dotenv()
 
 API_ID = os.getenv("API_ID", "27783899").strip()
 API_HASH = os.getenv("API_HASH", "30a0620127bd5816e9f5c69e1c426cf5").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7096733767:AAEvdhgUVDTpb62CUXwnrKSnd_0pdsUuj
-pk").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable
-anymore but you can add to get stats
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7096733767:AAEvdhgUVDTpb62CUXwnrKSnd_0pdsUujpk").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
 MUST_JOIN = os.getenv("MUST_JOIN", "@Reaper_Support")
 
 if not API_ID:
